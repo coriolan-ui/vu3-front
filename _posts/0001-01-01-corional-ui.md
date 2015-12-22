2d21d7c9e69b246a2d6328c0f83090bb647232cb
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-12-23 01:35:17 +0200

    add pages and active links

---
+++ _posts/0001-01-01-corional-ui.md
@@ -0,0 +1,5 @@
+---
+layout: coriolan-ui
+permalink: /coriolan-ui
+---
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    $('body').toggleClass('is-overflowed');
     e.preventDefault();
   });
+});
+
+$(function() {
+  $('.sidebar-menu a[href^="/coriolan-ui/' + location.pathname.split("/coriolan-ui/")[1] + '"]').addClass('active');
+  $('.sidebar-menu a[href^="/game-hall/' + location.pathname.split("/game-hall/")[1] + '"]').addClass('active');
 });
